e818a64b0b30b9f9eacffa94f043d2326a9c96c6
River Phillips <riverphillips1@gmail.com>
2023-10-20 11:36:43 +0100

    calculate hash

--- src/bencode/decoder.rs
+++ src/bencode/decoder.rs
@@ -1,6 +1,3 @@
-use std::io::Read;
-use serde_json::json;
-
 fn split_bytes_at(bytes: &[u8], at: char) -> Option<(&[u8], &[u8])> {
     let pos = bytes.iter().position(|&b| b == at as u8);
     if let Some(pos) = pos {
@@ -13,10 +10,15 @@ fn split_bytes_at(bytes: &[u8], at: char) -> Option<(&[u8], &[u8])> {
 fn decode_bencoded_number(encoded_value: &[u8]) -> (serde_json::Value, &[u8]) {
     match split_bytes_at(&encoded_value[1..], 'e') {
         Some((number_string, remaining)) => {
-            let number = number_string.iter().map(|&b| b as char).collect::<String>().parse::<i64>().unwrap();
+            let number = number_string
+                .iter()
+                .map(|&b| b as char)
+                .collect::<String>()
+                .parse::<i64>()
+                .unwrap();
             (number.into(), remaining)
-        },
-        None => panic!("Invalid encoded number: {:?}", encoded_value )
+        }
+        None => panic!("Invalid encoded number: {:?}", encoded_value),
     }
 }
 
@@ -24,17 +26,25 @@ fn decode_bencoded_string(encoded_value: &[u8]) -> (serde_json::Value, &[u8]) {
     // Example: "5:hello" -> "hello"
     match split_bytes_at(encoded_value, ':') {
         Some((number_string, string)) => {
-            let number = number_string.iter().map(|&b| b as char).collect::<String>().parse::<i64>().unwrap();
-            (String::from_utf8_lossy(&string[..number as usize]).into(), &string[number as usize..])
-        },
-        None => panic!("Invalid encoded string: {:?}", encoded_value )
+            let number = number_string
+                .iter()
+                .map(|&b| b as char)
+                .collect::<String>()
+                .parse::<i64>()
+                .unwrap();
+            (
+                String::from_utf8_lossy(&string[..number as usize]).into(),
+                &string[number as usize..],
+            )
+        }
+        None => panic!("Invalid encoded string: {:?}", encoded_value),
     }
 }
 
 fn decode_bencoded_list(encoded_value: &[u8]) -> (serde_json::Value, &[u8]) {
     let mut result = Vec::new();
     let mut remaining = &encoded_value[1..];
-    while remaining.iter().next() != Some(&b'e')  && remaining.len() > 0 {
+    while remaining.iter().next() != Some(&b'e') && remaining.len() > 0 {
         let (decoded_value, rem) = decode_bencoded_val(remaining);
         remaining = rem;
         result.push(decoded_value);
@@ -47,7 +57,7 @@ fn decode_bencoded_dict(encoded_value: &[u8]) -> (serde_json::Value, &[u8]) {
     let mut result = serde_json::Map::new();
 
     let mut remaining = &encoded_value[1..];
-    while remaining.iter().next() != Some(&b'e') && remaining.len() > 0{
+    while remaining.iter().next() != Some(&b'e') && remaining.len() > 0 {
         let (key, rem) = decode_bencoded_string(remaining);
         let (value, rem) = decode_bencoded_val(rem);
         result.insert(key.as_str().unwrap().to_string(), value);
@@ -63,8 +73,8 @@ fn decode_bencoded_val(encoded_value: &[u8]) -> (serde_json::Value, &[u8]) {
         Some(b'i') => decode_bencoded_number(encoded_value),
         Some(b'l') => decode_bencoded_list(encoded_value),
         Some(b'd') => decode_bencoded_dict(encoded_value),
-        _ => panic!("Unhandled encoded value: {:?}", encoded_value)
-    }   
+        _ => panic!("Unhandled encoded value: {:?}", encoded_value),
+    }
 }
 
 pub fn decode_bencoded_value(encoded_value: &[u8]) -> serde_json::Value {
@@ -72,54 +82,67 @@ pub fn decode_bencoded_value(encoded_value: &[u8]) -> serde_json::Value {
     decoded_value
 }
 
-// Add some tests 
+// Add some tests
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde_json::json;
+
 
     #[test]
-    fn test_decode_bencoded_int(){
+    fn test_decode_bencoded_int() {
         assert_eq!(decode_bencoded_value("i3e".as_bytes()), json!(3));
     }
 
     #[test]
-    fn test_decode_bencoded_negative_int(){
+    fn test_decode_bencoded_negative_int() {
         assert_eq!(decode_bencoded_value("i-3e".as_bytes()), json!(-3));
     }
 
     #[test]
-    fn test_decode_bencoded_string(){
+    fn test_decode_bencoded_string() {
         assert_eq!(decode_bencoded_value("5:hello".as_bytes()), json!("hello"));
     }
 
     #[test]
-    fn test_decode_bencoded_list(){
-        assert_eq!(decode_bencoded_value("l5:helloi52ee".as_bytes()), json!(["hello", 52]));
+    fn test_decode_bencoded_list() {
+        assert_eq!(
+            decode_bencoded_value("l5:helloi52ee".as_bytes()),
+            json!(["hello", 52])
+        );
     }
 
     #[test]
-    fn test_decode_empty_list(){
+    fn test_decode_empty_list() {
         assert_eq!(decode_bencoded_value("le".as_bytes()), json!([]));
     }
 
     #[test]
-    fn test_decode_simple_dict(){
-        assert_eq!(decode_bencoded_value("d3:foo3:bar5:helloi52ee".as_bytes()), json!({"foo": "bar", "hello": 52}));
+    fn test_decode_simple_dict() {
+        assert_eq!(
+            decode_bencoded_value("d3:foo3:bar5:helloi52ee".as_bytes()),
+            json!({"foo": "bar", "hello": 52})
+        );
     }
 
     #[test]
-    fn test_decode_empty_dict(){
+    fn test_decode_empty_dict() {
         assert_eq!(decode_bencoded_value("de".as_bytes()), json!({}));
     }
 
     #[test]
-    fn test_decode_nested_dict(){
-        assert_eq!(decode_bencoded_value("d10:inner_dictd4:key16:value14:key2i42e8:list_keyl5:item15:item2i3eeee".as_bytes()), json!({
-             "inner_dict":{
-                "key1":"value1",
-                "key2":42,
-                "list_key":["item1","item2",3]
-             }
-         }));
+    fn test_decode_nested_dict() {
+        assert_eq!(
+            decode_bencoded_value(
+                "d10:inner_dictd4:key16:value14:key2i42e8:list_keyl5:item15:item2i3eeee".as_bytes()
+            ),
+            json!({
+                "inner_dict":{
+                   "key1":"value1",
+                   "key2":42,
+                   "list_key":["item1","item2",3]
+                }
+            })
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+use serde::{Deserialize, Serialize};
 use std::{env, fs, str};
-use serde::Deserialize;
+use serde_bytes::ByteBuf;
+use sha1::{Sha1, Digest};
 
-mod parser;
+mod bencode;
 // Available if you need it!
 // use serde_bencode
-
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
@@ -14,35 +15,42 @@ fn main() {
     match command.as_str() {
         "decode" => {
             let encoded_value = &args[2];
-            let decoded_value = parser::parser::decode_bencoded_value(encoded_value.as_bytes());
+            let decoded_value = bencode::decoder::decode_bencoded_value(encoded_value.as_bytes());
             println!("{}", decoded_value.to_string());
         }
         "info" => {
             let torrent_file = &args[2];
 
             if let Ok(content) = fs::read(torrent_file) {
-                let decoded_value = parser::parser::decode_bencoded_value(&content.as_slice());
+                let decoded_value = bencode::decoder::decode_bencoded_value(&content.as_slice());
                 let torrent: Torrent = serde_json::from_value(decoded_value).unwrap();
+                let serialized = serde_bencode::to_string(&torrent.info).unwrap();
+
+                let mut hasher = Sha1::new();
+                hasher.update(serialized.as_bytes());
+
+                let info_hash = hasher.finalize();
+
                 println!("Tracker URL: {}", torrent.announce);
                 println!("Length: {}", torrent.info.length);
+                println!("Info hash: {}", hex::encode(info_hash));
             }
-
         }
-        _ => println!("unknown command: {}", command)
+        _ => println!("unknown command: {}", command),
     }
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Serialize)]
 struct Torrent {
     announce: String,
-    info: Info
+    info: Info,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Serialize)]
 struct Info {
     length: u64,
     name: String,
     #[serde(alias = "piece length")]
     piece_length: u64,
-    pieces: String
+    pieces: ByteBuf,
 }
